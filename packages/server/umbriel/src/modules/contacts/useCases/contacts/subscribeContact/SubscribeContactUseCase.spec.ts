e0f99b6761017a4f6ad769c6db16b08e05730be6
iamrosada <luisrosada2018@gmail.com>
2024-02-20 16:32:30 +0300

    docs and chores: change the name of example

--- packages/server/umbriel/src/modules/contacts/useCases/contacts/subscribeContact/SubscribeContactUseCase.spec.ts
+++ packages/server/umbriel/src/modules/contacts/useCases/contacts/subscribeContact/SubscribeContactUseCase.spec.ts
@@ -9,7 +9,7 @@ describe('Subscribe Contact', () => {
 
   it('should be able to subscribe new contact', async () => {
     await subscribeContactUseCase.execute({
-      email: 'diego@rocketseat.com.br',
+      email: 'luisrosada@mail.ru',
       tags: [
         { title: 'GoStack 10.0', integrationId: '123456' },
         { title: 'GoStack 11.0', integrationId: '441234' },
